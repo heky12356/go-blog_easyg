a570f0f093497ccf242a7ffdfe05ef93c78f2ea6
heky12356 <heji12356@gmail.com>
2025-04-06 13:06:40 +0800

    frontend fix 1. delete 页面基本功能写好了

--- easyg_frontend/README.md
+++ easyg_frontend/README.md
@@ -1,9 +1,12 @@
 页面
 
-home /
-about /about
-post /post
-    article /post/:uid
-admin /admin
-    create /admin/create
-    delete /admin/delete
+home /  
+about /about  
+post /post  
+    article /post/:uid  
+admin /admin  
+    create /admin/create  
+    delete /admin/delete  
+
+问题：
+当post为null时会访问失败，或许可以在api处返回空列表
--- easyg_frontend/src/pages/admin/delete.jsx
+++ easyg_frontend/src/pages/admin/delete.jsx
@@ -5,10 +5,16 @@ import { useState, useEffect } from "react";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 export default function Delete() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [iserror, setIserror] = useState(false);
+  const [uid, setUid] = useState(null);
   useEffect(() => {
     axios
       .get("/api/test/getposts")
@@ -26,9 +32,32 @@ export default function Delete() {
   if (error) return <div>发生错误...</div>;
   //console.log(data);
 
-  const handleClose = (e) => {
-    e.preventDefault();
-    console.log(1);
+  const deletepost = async () => {
+    //console.log(uid);
+    try {
+        await axios.delete(`/api/test/delete/${uid}`);
+        setData(data.filter(post => post.uid !== uid));
+        setIserror(true)
+        setModalMessage("删除成功!");
+        setUid(null);
+      } catch (error) {
+        //console.error("Error deleting post:", error);
+        setIserror(true);
+        setShowModal(true);
+        setModalMessage(error.response.data.error);
+      }    
+  };
+
+  const handleDelete = (post) => {
+    //console.log(post);
+    setShowModal(true);
+    setModalMessage("确定删除" + post.title + "吗?");
+    setUid(post.uid);
+  }
+
+  const handleClose = () => {
+    setShowModal(false);
+    setModalMessage("");
   }
 
   return (
@@ -43,7 +72,7 @@ export default function Delete() {
                 
               </Col>
               <Col md={3}>
-                <span onClick={handleClose} style={{cursor:"pointer"}}>x</span>
+                <span onClick={() => handleDelete(post)} style={{cursor:"pointer"}}>x</span>
               </Col>
             </Row>
           </Container>
@@ -52,6 +81,21 @@ export default function Delete() {
           <hr className="w-50"/>
           </div>
       ))}
+      
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Message</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{modalMessage}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={handleClose} className={iserror ? "d-none" : ""}>
+            取消
+          </Button>
+          <Button variant="danger" onClick={iserror ? handleClose : deletepost}>
+            确定
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div> 
   );
 }
