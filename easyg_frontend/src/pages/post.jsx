9bc13a5dbbbd4116f635a394b02fb88e8109c837
heky12356 <heji12356@gmail.com>
2025-04-18 21:26:37 +0800

    frontend add(re-commit): 1. 添加对markdown的支持，包括代码高亮，表格等

--- easyg_frontend/src/pages/post.jsx
+++ easyg_frontend/src/pages/post.jsx
@@ -3,9 +3,9 @@ import Container from "react-bootstrap/Container";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Markdown from "react-markdown";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
+import Mark from "../conponents/mark";
 export default function Post() {
   const { uid } = useParams();
   const [data, setData] = useState(null);
@@ -31,17 +31,17 @@ export default function Post() {
   if (loading) return <div>加载中...</div>;
   if (error) return <div>发生错误...</div>;
   //console.log(uid);
-  //console.log(data);
+  //console.log(data.content);
 
   if (data) {
     return (
       <div style={{ height: "100%" }}>
         <div className="h-75">
           <h2 className="text-center pb-3 mb-3">{data.title}</h2>
-          <Container className="" style={{overflowWrap: "break-word" }}>
+          <Container className="" style={{ overflowWrap: "break-word" }}>
             <Row className="justify-content-md-center">
-              <Col md={{span: 8}}>
-              <Markdown>{data.content}</Markdown>
+              <Col md={{ span: 8 }}>
+                <Mark>{data.content}</Mark>
               </Col>
             </Row>
           </Container>
