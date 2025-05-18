bb1578d9e55a9af626d064a3ebb4a45a50fce82c
heky12356 <heji12356@gmail.com>
2025-05-18 12:56:46 +0800

    style(loading): 使用Spinner组件替换简单文本加载提示
    为了提高加载状态的可视化效果，使用react-bootstrap的Spinner组件替换原有的简单文本提示，并添加样式以居中显示

--- easyg-frontend/src/app/loading.tsx
+++ easyg-frontend/src/app/loading.tsx
@@ -1,8 +1,21 @@
 "use client"
-export default function Loading() {
-    return (
-        <div>
-            Loading...
-        </div>
-    );
+import Spinner from 'react-bootstrap/Spinner';
+
+function Loading() {
+  return (
+    <div style={
+      {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "20vh",
+      }
+    }>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
+  );
 }
+
+export default Loading;
